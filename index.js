ac62225b876e5561bada4350e7e9aab28f220119
Clémence CHOMEL <clemence.chomel@gmail.com>
2022-05-27 19:51:47 +0200

    Removing cookie logging

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ app.get("/callback", function (req, res) {
   var code = req.query.code || null;
   var state = req.query.state || null;
   var storedState = req.cookies ? req.cookies[stateKey] : null;
-  console.log("cookies: " + JSON.stringify(req.cookies));
 
   if (state === null || state !== storedState) {
     res.redirect(
